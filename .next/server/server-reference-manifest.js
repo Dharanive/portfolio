648162ab057b34dddb111a46587c62fff7053898
Dharanive <20104022@hicet.ac.in@gamil.com>
2025-07-08 16:30:31 +0530

    bug fix

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"0zGgnocgHwG2YQS/zytsgktZBeASWOhnAR5w45OfxmA=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"}"
--- .next/static/chunks/src_app_page_tsx_e10cff8f._.js
+++
@@ -1,9 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/src_1eb70b28._.js",
-  "static/chunks/node_modules_05159b6d._.js",
-  "static/chunks/src_components_ProfileCard_ProfileCard_5523da45.css"
-],
-    source: "dynamic"
-});
